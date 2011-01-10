4555dedcf8696e4b205596237d288872734d6384
André Santos <andrefs@cpan.org>
2011-01-10 18:25:58 +0000

    minor change

--- linux1.sh
+++ linux1.sh
@@ -2,8 +2,8 @@
 
 # Copy(replace) vim files.
 
-echo "Files are being copied!"
+echo "Vim-related files are being copied!"
 cp -rf $UTILS_PROJECT_PATH/HOME/.vim ~/
 cp -rf $UTILS_PROJECT_PATH/HOME/.vimrc ~/.vimrc
 mkdir ~/.vim/backups ~/.vim/tmp
-echo "done!"
+echo "Done!"
