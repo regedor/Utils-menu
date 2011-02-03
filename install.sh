3e835526ed2a700144c6721f93fa206b39b26e88
André Santos <andrefs@cpan.org>
2011-02-03 23:24:56 +0000

    fixed paths in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$UTILS_PROJECT_PATH" = "" ] ; then
   echo "## Regedor Utils Scripts."                                                                           >> ~/.bashrc
-  echo "export UTILS_PROJECT_PATH=\"$(dirname $PWD/$0)\""                                                    >> ~/.bashrc
-  echo "source `dirname $PWD/$0`/BASHRC/bashrc"                                                              >> ~/.bashrc
+  echo "export UTILS_PROJECT_PATH=\"$(dirname $0)\""                                                    >> ~/.bashrc
+  echo "source `dirname $0`/BASHRC/bashrc"                                                              >> ~/.bashrc
   echo  export PS1="'"'\[\033[0;35m\]\h \[\033[0;33m\]\w\[\033[0;32m\] $(parse_git_branch)\[\033[00m\]: '"'" >> ~/.bashrc
   echo "Restart your bash and run utils-menu."
 else
