a652acbaae77468fc903976a0b9fb7f761ff80ab
André Santos <andrefs@cpan.org>
2011-08-30 17:48:42 +0100

    installer almost finished

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ else
 	  BASHRC_PATH="~/.bash_profile"	
 	  BASHRC_FILE="bashrc_mac"
 	else	
-	  BASHRC_PATH="~/.bashrc"
+	  BASHRC_PATH=~/.bashrc
 	  BASHRC_FILE="bashrc"	
 	fi
 	
@@ -36,10 +36,9 @@ else
 	echo "*** Creating directory '$UTILS_PROJECT_PATH'..."
 	mkdir -p "$UTILS_PROJECT_PATH"
 	
-	mkdir -p "$GBINSTALLDIR"
-
 	echo "*** Cloning Utils-menu Git repository to '$UTILS_PROJECT_PATH'..."
 	git clone git://github.com/regedor/Utils-menu.git "$UTILS_PROJECT_PATH" 2>/dev/null
+	chmod -R 755 "$UTILS_PROJECT_PATH"
 	cd "$UTILS_PROJECT_PATH"
 
 	git submodule init 2>/dev/null
@@ -48,23 +47,44 @@ else
 	git submodule foreach git pull 2>/dev/null
 
 	# Add $UTILS_PROJECT_PATH to bashrc
-	cat <<- END > "$BASHRC_PATH"
+	# cat <<- END >> $BASHRC_PATH
 
-	## Utils Menu Path."                         
-	export UTILS_PROJECT_PATH="$UTILS_PROJECT_PATH"
-	END
+	# ## Utils Menu Path."                         
+	# export UTILS_PROJECT_PATH="$UTILS_PROJECT_PATH"
+	# END
 
-	DMINSTALLDIR="$UTILS_PROJECT_PATH/Vendor/dirmenu"
+	export DMINSTALLDIR="$UTILS_PROJECT_PATH/Vendor/dirmenu"
 	cd "$DMINSTALLDIR"
 	bash "$DMINSTALLDIR"/dirmenu_generate
 
 	# Add utils-menu to /usr/local/bin
 	cat <<- END > /usr/local/bin/utils-menu
+	#!/bin/bash
+
 	export UTILS_PROJECT_PATH="$UTILS_PROJECT_PATH"
+
+	UTILSDIROWNER=\$(ls -ld "$UTILS_PROJECT_PATH" | awk '{ print \$3}')
+	UTILSUSER=\$(echo \$USER)
+	if [ "\$UTILSDIROWNER" != "\$UTILSUSER" ]; then
+		echo "You need to own '$UTILS_PROJECT_PATH' to run utils-menu,"
+		echo "but the current owner is '\$UTILSDIROWNER'!"
+		echo
+		echo "Do you want to change the ownership of '$UTILS_PROJECT_PATH'?"
+		read -p "(sudo password will be needed) [y/N] "
+		if [[ "\$REPLY" == "y" || "\$REPLY" == "Y" ]]; then
+			sudo chown -R "\$UTILSUSER":"\$UTILSUSER" "$UTILS_PROJECT_PATH"
+		else
+			echo
+			echo utils-menu aborted!
+		fi
+	fi
+
 	export dirmenu="$UTILS_PROJECT_PATH/Vendor/dirmenu/dirmenu"
 	"$UTILS_PROJECT_PATH"/Menu/utils-menu/configurations/auto_update_um.sh
 	dirmenu "$UTILS_PROJECT_PATH"/Menu/utils-menu
 	END
+
+	chmod +x /usr/local/bin/utils-menu
 
 fi
 
