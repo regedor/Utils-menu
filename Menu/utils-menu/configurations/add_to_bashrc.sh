229431731a2ed3e9ae49151bcdb0fb9d82a20e97
André Santos <andrefs@cpan.org>
2011-09-19 15:19:24 +0100

    new install.sh

---
+++ Menu/utils-menu/configurations/add_to_bashrc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Queres por isto no teu bashrc? [Y/n] "
+if [[ "$REPLY"=="n" || "$REPLY"=="N" ]]; then
+	exit 1
+fi
+
+# Choose bashrc depending on OS
+if [ `uname` == "Darwin" ] ; then
+  BASHRC_PATH="$HOME/.bash_profile" 
+  BASHRC_FILE="bashrc_mac"
+else    
+  BASHRC_PATH="$HOME/.bashrc"
+  BASHRC_FILE="bashrc"  
+fi  
+
+echo 'source "$UTILS_PROJECT_PATH"/BASHRC/$BASHRC_FILE"' >> "$BASHRC_PATH"
+
+exit 0
