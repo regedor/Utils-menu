58587d87920e6f1d0dcf283318918fe79d269937
Miguel Regedor <miguelregedor@gmail.com>
2011-10-05 19:09:25 +0100

    script to open our web sites added

---
+++ Menu/utils-menu/group_buddies/open_all_handy_ants.sh
@@ -0,0 +1,15 @@
+echo "** Here we go!"
+open http://www.adege.eeg.uminho.pt
+sleep 6 ; open http://www.ajtg.pt
+sleep 6 ; open http://www.atrevete.eu
+sleep 6 ; open http://www.cesium.di.uminho.pt
+sleep 6 ; open http://www.gaccum.com
+sleep 6 ; open http://www.gaeb.pt
+sleep 6 ; open http://www. groupbuddies.com
+sleep 6 ; open http://www.leftright.pt
+sleep 6 ; open http://www.nebaum.bio.uminho.pt
+sleep 6 ; open http://www.nebioquc.com
+sleep 6 ; open http://www.necc.pt
+sleep 6 ; open http://www.nemum.com
+echo "** Done."
+echo
