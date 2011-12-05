5f433c06cebf392698d875bb8b1b1baff0dd94fe
Miguel Regedor <miguelregedor@gmail.com>
2011-12-05 17:44:19 +0000

    list of sites refreshed

--- Menu/utils-menu/group_buddies/open_all_handy_ants.sh
+++ Menu/utils-menu/group_buddies/open_all_handy_ants.sh
@@ -1,16 +1,19 @@
 echo "** Here we go!"
 open http://www.sitetree.groupbuddies.com
-sleep 6 ; open http://www.adege.eeg.uminho.pt
-sleep 6 ; open http://www.ajtg.pt
-sleep 6 ; open http://www.atrevete.eu
-sleep 6 ; open http://www.cesium.di.uminho.pt
-sleep 6 ; open http://www.gaccum.com
-sleep 6 ; open http://www.gaeb.pt
-sleep 6 ; open http://www. groupbuddies.com
-sleep 6 ; open http://www.leftright.pt
-sleep 6 ; open http://www.nebaum.bio.uminho.pt
-sleep 6 ; open http://www.nebioquc.com
-sleep 6 ; open http://www.necc.pt
-sleep 6 ; open http://www.nemum.com
+sleep 9 ; open http://www.adege.eeg.uminho.pt
+sleep 9 ; open http://www.ajtg.pt
+sleep 9 ; open http://www.atrevete.eu
+sleep 9 ; open http://www.cesium.di.uminho.pt
+sleep 9 ; open http://www.gaccum.com
+sleep 9 ; open http://www.gaeb.pt
+sleep 9 ; open http://www.groupbuddies.com
+sleep 9 ; open http://www.leftright.pt
+sleep 9 ; open http://www.nebaum.bio.uminho.pt
+sleep 9 ; open http://www.nebioquc.com
+sleep 9 ; open http://www.necc.pt
+sleep 9 ; open http://www.nemum.com
+sleep 9 ; open http://www.nedac.com
+sleep 9 ; open http://www.parisportugues.com
+sleep 9 ; open http://psd.groupbuddies.com
 echo "** Done."
 echo
