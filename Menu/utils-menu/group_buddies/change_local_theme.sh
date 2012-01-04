8cd380fccfa346f49bb586ea3da31b4a38207a17
Miguel Regedor <miguelregedor@gmail.com>
2012-01-04 14:59:04 +0000

    changes to opena all sites. And, css remove added to change theme

--- Menu/utils-menu/group_buddies/change_local_theme.sh
+++ Menu/utils-menu/group_buddies/change_local_theme.sh
@@ -9,6 +9,9 @@ fi
 LOCALCONFIG="$PROJECTDIR"/config/config.local
 THEMESPROJECTPATH=$(grep themes_project_path "$PROJECTDIR"/config/config.local | perl -p -e 's/.*"(.*)"/$1/')
 
+rm "$PROJECTDIR"/public/stylesheets/frontend.css
+rm "$PROJECTDIR"/public/stylesheets/backend.css
+
 echo "Calling '$UTILS_PROJECT_PATH/Menu/utils-menu/group_buddies/change_theme.pl' '$LOCALCONFIG' '$THEMESPROJECTPATH' '$PROJECTDIR'..."
 "$UTILS_PROJECT_PATH"/Menu/utils-menu/group_buddies/change_theme.pl "$LOCALCONFIG" "$THEMESPROJECTPATH" "$PROJECTDIR"
 echo "Theme has been updated!"
--- Menu/utils-menu/group_buddies/open_all_handy_ants.sh
+++ Menu/utils-menu/group_buddies/open_all_handy_ants.sh
@@ -6,14 +6,15 @@ sleep 9 ; open http://www.atrevete.eu
 sleep 9 ; open http://www.cesium.di.uminho.pt
 sleep 9 ; open http://www.gaccum.com
 sleep 9 ; open http://www.gaeb.pt
-sleep 9 ; open http://www.groupbuddies.com
 sleep 9 ; open http://www.leftright.pt
 sleep 9 ; open http://www.nebaum.bio.uminho.pt
 sleep 9 ; open http://www.nebioquc.com
 sleep 9 ; open http://www.necc.pt
 sleep 9 ; open http://www.nemum.com
 sleep 9 ; open http://www.nedaac.com
-sleep 9 ; open http://www.parisportugues.com
-sleep 9 ; open http://psd.groupbuddies.com
+sleep 9 ; open http://www.parisportugues.com/posts
+sleep 9 ; open http://psd.groupbuddies.com/posts
+sleep 9 ; open http://www.sentirsaude.pt/posts
+sleep 9 ; open http://www.groupbuddies.com/posts
 echo "** Done."
 echo
