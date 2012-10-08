e565f9db0a491f897e5427b0e221a09b9941a99e
André Santos <andrefs@cpan.org>
2012-10-08 10:03:42 +0100

    add -f to rm in change_local_theme

--- Menu/utils-menu/group_buddies/change_local_theme.sh
+++ Menu/utils-menu/group_buddies/change_local_theme.sh
@@ -9,8 +9,8 @@ fi
 LOCALCONFIG="$PROJECTDIR"/config/config.local
 THEMESPROJECTPATH=$(grep themes_project_path "$PROJECTDIR"/config/config.local | perl -p -e 's/.*"(.*)"/$1/')
 
-rm "$PROJECTDIR"/public/stylesheets/frontend.css
-rm "$PROJECTDIR"/public/stylesheets/backend.css
+rm -f "$PROJECTDIR"/public/stylesheets/frontend.css
+rm -f "$PROJECTDIR"/public/stylesheets/backend.css
 
 echo "Calling '$UTILS_PROJECT_PATH/Menu/utils-menu/group_buddies/change_theme.pl' '$LOCALCONFIG' '$THEMESPROJECTPATH' '$PROJECTDIR'..."
 "$UTILS_PROJECT_PATH"/Menu/utils-menu/group_buddies/change_theme.pl "$LOCALCONFIG" "$THEMESPROJECTPATH" "$PROJECTDIR"
